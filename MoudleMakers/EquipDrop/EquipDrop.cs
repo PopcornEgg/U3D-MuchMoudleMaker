80073e9f2146ed2d14b79b922977e279b7ff3d4a
PopcornEgg <251197161@qq.com>
2016-03-17 17:21:00 +0800

    装备掉落+动画
    装备掉落+动画

--- MoudleMakers/EquipDrop/EquipDrop.cs
+++ MoudleMakers/EquipDrop/EquipDrop.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Drop : MonoBehaviour {
+public class EquipDrop : MonoBehaviour {
 
-    static Random rander = new Random();
+    public static Vector3[] dropPositions;
+    static EquipDrop()
+    {
+        float dis = 1.0f;
+        dropPositions = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(-dis, 0, dis),
+            new Vector3(0, 0, dis),
+            new Vector3(dis, 0, dis),
+            new Vector3(dis, 0, 0),
+            new Vector3(dis, 0, -dis),
+            new Vector3(0, 0, -dis),
+            new Vector3(-dis, 0, -dis),
+            new Vector3(-dis, 0, 0),
+        };
+//         for (int i = 0; i < 5; i++)
+//         {
+//             for (int j = 0; j < 5; j++)
+//             {
+//                 dropPositions[i * 5 + j] = Vector3.zero + new Vector3(i * dis, 0.0f, j * dis) - new Vector3(dis*2, 0, dis * 2);
+//             }
+//         }
+    }
     public GameObject[] dropList;
 	// Use this for initialization
 	void Start () {
@@ -18,7 +41,32 @@ public class Drop : MonoBehaviour {
 
    // Can't destroy Transform component of 'Cylinder'. If you want to destroy the game object, please call 'Destroy' on the game object instead. Destroying the transform component is not allowed.
 
-    public void Died() {
+    public void Die() {
+
+        //if (dropList == null)
+        //    return;
+
+        //Debug.Log("in Died");
+        //Destroy(this);
+        //DestroyImmediate(gameObject);
+
+        //UnityEngine.Object obj = Resources.Load("sword");
+        //Instantiate(obj);
+        //Instantiate<GameObject>("sword");
+
+        int dropNum = Random.Range(1, dropPositions.Length + 1);
+        for (int i = 0; i < dropNum; i++)
+        {
+            GameObject obj = Instantiate<GameObject>(dropList[Random.Range(0, dropList.Length)]);
+            obj.transform.position = transform.position + dropPositions[i];
+            obj.AddComponent<SelfRation>();
+            //Instantiate(dropList[Random.Range(0, dropList.Length)], transform.position, transform.rotation);
+        }
+    }
+
+    public GameObject testObj;
+    public void DiedII()
+    {
 
         //if (dropList == null)
         //    return;
@@ -32,18 +80,37 @@ public class Drop : MonoBehaviour {
         //Instantiate<GameObject>("sword");
 
 
+        //画一个园
+        /*
+        float dis = 2.0f;
+        Vector3 newPos = transform.position ;
+        newPos.x += dis;
+        int dropMaxCount = 8;
+        int currDropCount = UnityEngine.Random.Range(1, dropMaxCount + 1);
+        float aangle = 360.0f / (float)currDropCount;
+        for (int i = 0; i < currDropCount; i++) {
 
-        int dropNum = Random.Range(1, 5);
-        Debug.Log("src   Random :" + dropNum);
-        while (dropNum-- > 0) {
+           // Quaternion.LookRotation(transform.position);
+           // Instantiate(dropList[Random.Range(0, dropList.Length)], transform.position, Quaternion.zero);
+            GameObject obj = Instantiate<GameObject>(testObj);
 
-            // Debug.Log("Random :" + dropNum);
-            transform.Rotate(Vector3.up, 30.0f);
-           // transform.rotation.rat
-            Instantiate(dropList[Random.Range(0, dropList.Length)], transform.position, transform.rotation);
+            obj.transform.position = newPos;
+            obj.transform.RotateAround(transform.position, Vector3.up, aangle * i);
+            //obj.pos
+        }
+        */
+
+        for (int i = 0; i < dropPositions.Length; i++) {
+
+           // Quaternion.LookRotation(transform.position);
+           // Instantiate(dropList[Random.Range(0, dropList.Length)], transform.position, Quaternion.zero);
+            GameObject obj = Instantiate<GameObject>(testObj);
+
+            obj.transform.position = transform.position + dropPositions[i];
+            //obj.transform.RotateAround(transform.position, Vector3.up, aangle * i);
+            //obj.pos
         }
 
-        
     }
 
 }
---
+++ MoudleMakers/Utils/SelfRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfRation : MonoBehaviour {
+
+    public float speed = 100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.up, Time.deltaTime * speed);
+	}
+}

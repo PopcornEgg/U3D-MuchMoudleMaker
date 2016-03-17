9366e7f500600082b70e04a3f1b2ac5fa8a1e19b
PopcornEgg <251197161@qq.com>
2016-03-17 15:13:48 +0800

    背包v2
    背包v2

---
+++ MoudleMakers/UIs/MUIControls/MUIScrollView.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class MUIScrollView : MonoBehaviour
+{
+    RectTransform content;//内容
+    GridLayoutGroup grid;
+    ScrollRect scrollRect;
+
+    public RectOffset padding = new RectOffset(0, 0, 0, 0);
+    public Vector2 cellSize = Vector2.one;
+    public Vector2 spacing = Vector2.one;
+    public GridLayoutGroup.Constraint constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+    public int constraintCount = 1;
+    public bool horizontal = false;
+    public bool vertical = false;
+
+    void Start () {
+
+        scrollRect = GetComponent<ScrollRect>();
+
+        if (scrollRect != null) {
+
+            scrollRect.horizontal = horizontal;
+            scrollRect.vertical = vertical;
+            content = scrollRect.content;
+
+            grid = content.GetComponent<GridLayoutGroup>();
+            grid.spacing = spacing;
+            grid.padding = padding;
+            grid.constraint = constraint;
+            grid.constraintCount = constraintCount;
+        }
+    }
+
+    public void AddItem(GameObject obj)
+    {
+        if (obj != null) {
+            obj.transform.SetParent(content, false);
+        }
+    }
+    public GameObject GetItem(int idx)
+    {
+        if (idx >= 0 && idx < content.childCount)
+            return content.GetChild(idx).gameObject;
+
+        return null;
+    }
+    public void DelItem(int idx, bool destory = true)
+    {
+        GameObject obj = GetItem(idx);
+        if (obj != null)
+        {
+            obj.SetActive(false);
+            if (destory)
+            {
+                DestroyImmediate(obj);
+            }
+        }
+    }
+}
---
+++ MoudleMakers/UIs/PlayerBag/PlayerBag.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerBag : MonoBehaviour
+{
+    public int bagSpace = 100;//背包容量
+    public GameObject itemObj;
+
+    RectTransform content;//内容
+    GridLayoutGroup grid;
+    ScrollRect scrollRect;
+
+    void Start()
+    {
+        if (itemObj == null)
+            return;
+
+        Transform tfMain = transform.FindChild("Main");
+
+        scrollRect = tfMain.GetComponent<ScrollRect>();
+        content = scrollRect.content;
+        grid = content.GetComponent<GridLayoutGroup>();
+
+        //设置item高宽
+        RectTransform rtfViewport = tfMain.FindChild("Viewport").GetComponent<RectTransform>();
+        float itemWH = (rtfViewport.rect.width - grid.spacing.x * (float)(grid.constraintCount - 1) - grid.padding.left - grid.padding.right) / grid.constraintCount;
+        grid.cellSize = new Vector2(itemWH, itemWH);
+
+        //设置content高度
+        int rowCount = bagSpace / grid.constraintCount;
+        float contentWH = itemWH * rowCount + grid.spacing.y * (float)(rowCount - 1) + grid.padding.top + grid.padding.bottom;
+        content.sizeDelta = new Vector2(0, contentWH);
+
+        for (int i = 0; i < bagSpace; i++)
+        {
+            GameObject cobj = Instantiate<GameObject>(itemObj);
+            cobj.SetActive(true);
+            cobj.transform.SetParent(content, false);
+        }
+        itemObj.SetActive(false);
+    }
+}

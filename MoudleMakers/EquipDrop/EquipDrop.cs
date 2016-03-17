b22d67842f42ceae81a02898b987bf0489f2f4db
PopcornEgg <251197161@qq.com>
2016-03-17 10:50:57 +0800

    root
    根版本

---
+++ MoudleMakers/EquipDrop/EquipDrop.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drop : MonoBehaviour {
+
+    static Random rander = new Random();
+    public GameObject[] dropList;
+	// Use this for initialization
+	void Start () {
+        //Died();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+
+   // Can't destroy Transform component of 'Cylinder'. If you want to destroy the game object, please call 'Destroy' on the game object instead. Destroying the transform component is not allowed.
+
+    public void Died() {
+
+        //if (dropList == null)
+        //    return;
+
+        //Debug.Log("in Died");
+        //Destroy(this);
+        //DestroyImmediate(gameObject);
+
+        //UnityEngine.Object obj = Resources.Load("sword");
+        //Instantiate(obj);
+        //Instantiate<GameObject>("sword");
+
+
+
+        int dropNum = Random.Range(1, 5);
+        Debug.Log("src   Random :" + dropNum);
+        while (dropNum-- > 0) {
+
+            // Debug.Log("Random :" + dropNum);
+            transform.Rotate(Vector3.up, 30.0f);
+           // transform.rotation.rat
+            Instantiate(dropList[Random.Range(0, dropList.Length)], transform.position, transform.rotation);
+        }
+
+        
+    }
+
+}
---
+++ MoudleMakers/PlayerBag/PlayerBag.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerBag : MonoBehaviour
+{
+    public GameObject contentObj;
+    public GameObject itemObj;
+
+    static int rowCount = 10;
+    static int columnCount = 5;
+
+    void Start()
+    {
+        if (itemObj == null)
+            return;
+
+        ScrollRect srt = transform.FindChild("Main").GetComponent<ScrollRect>();
+        float wh = 40.0f;
+        for (int r = 0; r < rowCount; r++)
+        {
+            for (int c = 0; c < columnCount; c++)
+            {
+                GameObject cobj = Instantiate<GameObject>(itemObj);
+                cobj.SetActive(true);
+
+                //cobj.en
+                cobj.transform.SetParent(contentObj.transform, false);
+                RectTransform rtf = cobj.GetComponent<RectTransform>();
+
+                rtf.anchoredPosition = new Vector2(c * wh, r * wh);
+            }
+        }
+        itemObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
